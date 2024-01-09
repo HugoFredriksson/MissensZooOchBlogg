000f333bec9b295d5e1387881cc22e063fa6391f
HugoFredriksson <hufr04001@utb.vaxjo.se>
2024-01-09 14:57:14 +0100

    jaja

---
+++ webbshop.js
@@ -0,0 +1,18 @@
+function init() {
+
+};
+window.onload = init;
+
+async function createProduct() {
+    json = {
+       "productId" : "productId",
+       "productDescription" : "productDescription",
+       "productName" : "productName",
+       "productCategory" : "productcategory",
+       "productPrice" : "productPrice",
+       "productStock" : "productStock",
+       "productImage" : "productImage" 
+    };
+    
+
+}
